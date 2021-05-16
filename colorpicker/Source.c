30bdd8621aed729c43ff442f192b88d204db24f5
system64 <cindytianla@gmail.com>
2021-05-15 18:55:56 -0700

    changed malloc

--- colorpicker/Source.c
+++ colorpicker/Source.c
@@ -106,7 +106,7 @@ int main(){
                 
                 //print RGB value
                 int bufferSize = snprintf(NULL, 0, "\nRGB: %i, %i, %i\n", cr, cg, cb);
-                const char* buffer = malloc(bufferSize + 1);
+                const char* buffer = malloc(bufferSize * sizeof(*buffer) + 1);
                 if(buffer == NULL){     //check if malloc failed
                     console_color_init();
                     printf("[-]Failed to allocate buffer 1 ...");
@@ -118,7 +118,7 @@ int main(){
 
                 //print HEX value
                 int buffer2Size = snprintf(NULL, 0, "Hex: #%x\n", hexcolor(cr, cg, cb) + bufferSize);
-                const char* buffer2 = malloc(buffer2Size + bufferSize + 1);
+                const char* buffer2 = malloc(buffer2Size * sizeof(*buffer2) + bufferSize + 1);
                 if (buffer2 == NULL) {      //check if malloc failed
                     console_color_init();
                     printf("[-]Failed to allocate buffer 2 ...");
